4bbf24bd7b3a1455711f5118d8f313faf88b5dc7
Chuhan Feng <simonrouse9461@gmail.com>
2017-10-19 15:31:56 -0400

    update

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import com.example.myfirstapp.discovery.DiscoveryService;
 import com.example.myfirstapp.logins.IFTTTLogin;
 import com.example.myfirstapp.logins.NestLogin;
 import com.example.myfirstapp.services.IFTTTService;
@@ -29,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         commandText = (EditText) findViewById(R.id.commandField);
         commandText.setText("");
-        DiscoveryService search = new DiscoveryService();
 
         service = new NaturalLanguageClassifier();
         service.setUsernameAndPassword("a475cc56-93c6-4b1c-9cc5-f76d8af50830", "rFlhaBf2aEtS");
@@ -74,18 +72,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void iftttLogin(View view) {
         startActivity(new Intent(this, IFTTTLogin.class));
-    }
-    public void discoverySearch(View view){
-
-        final String command = commandText.getText().toString();
-
-        startActivity(new Intent(this, DiscoveryActivity.class));
-
-
-
-
-
-
     }
 }
 
